fda449a47e30e2709dba9e0c96b16a0eff036469
Shivam <thisisshivam06@gmail.com>
2022-08-14 23:58:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// let a = "";
+// for (let i = 1; i <= 5; i++) {
+//   a = a + "*";
+//   console.log(a);
+// }
+
+// console.log("*****\n   * \n  *  \n *   \n*****");
+
+const x = 15;
+const y = 10;
+
+// const sum = x + y;
+
+// console.log(sum);
+// console.log("Sum of " + x + " and " + y + " is " + sum);
+
+// xy
+const prd = x * y;
+console.log("Product of " + x + " and " + y + " is " + prd);
+// x / y, y / x, x % y
+const v1 = x / y;
+const v2 = y / x;
+const v3 = x % y;
+console.log(v1);
+console.log(v2);
+console.log(v3);
